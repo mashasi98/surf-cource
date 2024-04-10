524643ca0985dc04588be30010f5ac6cfcb6652d
mashasi98 <mashasi98@yandex.ru>
2024-04-10 11:40:29 +0300

    Homework ,task 3 ,Травушка

---
+++ task-3/bin/surf_dart_courses_template.dart
@@ -0,0 +1,201 @@
+import 'dart:collection';
+
+enum Countries { brazil, russia, turkish, spain, japan }
+
+class Territory {
+  final int areaInHectare;
+  final List<String> crops;
+  final List<AgriculturalMachinery> machineries;
+
+  Territory(
+      this.areaInHectare,
+      this.crops,
+      this.machineries,
+      );
+}
+
+class AgriculturalMachinery {
+  final String id;
+  final DateTime releaseDate;
+
+  AgriculturalMachinery(
+      this.id,
+      this.releaseDate,
+      );
+
+  /// Переопределяем оператор "==", чтобы сравнивать объекты по значению.
+  @override
+  bool operator ==(Object? other) {
+    return identical(this, other) || (other is AgriculturalMachinery && other.id == id && other.releaseDate == releaseDate);
+  }
+  @override
+  int get hashCode => Object.hashAll([id, releaseDate]);
+}
+
+final mapBefore2010 = <Countries, List<Territory>>{
+  Countries.brazil: [
+    Territory(
+      34,
+      ['Кукуруза'],
+      [
+        AgriculturalMachinery(
+          'Трактор Степан',
+          DateTime(2001),
+        ),
+        AgriculturalMachinery(
+          'Культиватор Сережа',
+          DateTime(2007),
+        ),
+      ],
+    ),
+  ],
+  Countries.russia: [
+    Territory(
+      14,
+      ['Картофель'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Гранулятор Антон',
+          DateTime(2009),
+        ),
+      ],
+    ),
+    Territory(
+      19,
+      ['Лук'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Дробилка Маша',
+          DateTime(1990),
+        ),
+      ],
+    ),
+  ],
+  Countries.turkish: [
+    Territory(
+      43,
+      ['Хмель'],
+      [
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+        AgriculturalMachinery(
+          'Сепаратор Марк',
+          DateTime(2005),
+        ),
+      ],
+    ),
+  ],
+};
+
+final mapAfter2010 = {
+  Countries.turkish: [
+    Territory(
+      22,
+      ['Чай'],
+      [
+        AgriculturalMachinery(
+          'Каток Кирилл',
+          DateTime(2018),
+        ),
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+      ],
+    ),
+  ],
+  Countries.japan: [
+    Territory(
+      3,
+      ['Рис'],
+      [
+        AgriculturalMachinery(
+          'Гидравлический молот Лена',
+          DateTime(2014),
+        ),
+      ],
+    ),
+  ],
+  Countries.spain: [
+    Territory(
+      29,
+      ['Арбузы'],
+      [
+        AgriculturalMachinery(
+          'Мини-погрузчик Максим',
+          DateTime(2011),
+        ),
+      ],
+    ),
+    Territory(
+      11,
+      ['Табак'],
+      [
+        AgriculturalMachinery(
+          'Окучник Саша',
+          DateTime(2010),
+        ),
+      ],
+    ),
+  ],
+};
+
+void main() {
+  final List<AgriculturalMachinery> machineListBefore2010 =
+  Analyzer().getList(mapBefore2010);
+  final List<AgriculturalMachinery> machineListAfter2010 =
+  Analyzer().getList(mapAfter2010);
+  final List<AgriculturalMachinery> allMachine =
+  Analyzer().summaryList(machineListBefore2010, machineListAfter2010);
+  Analyzer().showAverageAge(allMachine, 'Средний возраст всей техники');
+  Analyzer().showAverageAge(Analyzer().getOldestPart(allMachine),
+      'Средний возраст самой старой техники');
+}
+
+class Analyzer {
+  List<AgriculturalMachinery> getList(Map<Countries, List<Territory>> map) {
+    return map.values
+        .expand((allTerritory) => allTerritory
+        .map((currentTerritory) => currentTerritory.machineries))
+        .expand((machines) => machines.toList())
+        .toList();
+  }
+
+  List<AgriculturalMachinery> summaryList(List<AgriculturalMachinery> fistList,
+      List<AgriculturalMachinery> secondList) {
+    return fistList + secondList;
+  }
+
+  void showAverageAge(List<AgriculturalMachinery> allMachine, String message) {
+    final averageAge = allMachine
+        .map((machine) =>
+        (DateTime.now().year - machine.releaseDate.year).toInt())
+        .reduce((value, element) => value + element) /
+        allMachine.length;
+    print('$message - ${averageAge.round()}.');
+  }
+
+  List<AgriculturalMachinery> getOldestPart(
+      List<AgriculturalMachinery> machineList) {
+    final sortedMachineList = getSortedByAgeUniqueMachine(machineList);
+    return machineList.sublist(0, (sortedMachineList.length ~/ 2).round());
+  }
+
+  SplayTreeSet<AgriculturalMachinery> getSortedByAgeUniqueMachine(
+      List<AgriculturalMachinery> machineList) {
+    final nonRepeatingMachine =
+    SplayTreeSet<AgriculturalMachinery>((a, b) => a.id.compareTo(b.id));
+    nonRepeatingMachine.addAll(machineList);
+    return nonRepeatingMachine;
+  }
+}
