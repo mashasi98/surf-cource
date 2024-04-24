ea34460243a2bcba9f010b1725e4c3bae77bf8ea
mashasi98 <mashasi98@yandex.ru>
2024-04-24 14:39:02 +0300

    Revert "Task-9. 6shop"
    This reverts commit a42bea06af9db97cccb712d39a3b201e2067c6e8.

--- task-9/bin/interface/filter.dart
+++
@@ -1,3 +0,0 @@
-abstract interface class Filter<T> {
-  bool filter_item(T item);
-}
--- task-9/bin/main.dart
+++
@@ -1,6 +0,0 @@
-import 'utils/itemUtils.dart';
-
-void main() {
-  var utils = ItemUtils();
-  utils.printCatalog(utils.getProductCatalog());
-}
